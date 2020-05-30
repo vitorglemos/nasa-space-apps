70b91664b9892e2b9b942540ad6dacaf1b1fcb03
gabriele <gabriieele6@gmail.com>
2020-05-30 17:47:37 -0300

    iniciando a página principal

---
+++ streamlit/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+
+def main():
+    st.title('Track Covid-19')
+    imagem = st.file_uploader('Upload your file', type='jpg')
+    if file is not None:
+
+
+if __name__ == '__main__':
+    main()
