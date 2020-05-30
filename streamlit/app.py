9254091e3cb0cf623225f638d3c614778a4ed6de
gabriele <gabriieele6@gmail.com>
2020-05-30 18:18:45 -0300

    requisitando imagens com sreamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 def main():
     st.title('Track Covid-19')
-    imagem = st.file_uploader('Upload your file', type='jpg')
-    if file is not None:
+    imagem = st.file_uploader('Upload your file', type='png')
+    if imagem is not None:
+        st.image(imagem)
 
 
 if __name__ == '__main__':
