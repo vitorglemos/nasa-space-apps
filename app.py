03876971313deafd0ac6382b255b97bfd7780327
Lucas Casimiro de Souza Vizeu <lucas.casimiro.vizeu@gmail.com>
2020-05-31 11:55:00 -0300

    deconde and enconding to base64

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import time
 import base64
 
 app = Flask(__name__)
-# s3 = boto3.resource("s3", aws_access_key_id="AKIAWKFFMIP2PRIVCDEV", aws_secret_access_key="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")
-# bucket = s3.Bucket("nasa-space-app")
 
 
 @app.route('/vision', methods=["POST"])
@@ -14,15 +12,15 @@ def vision():
     # pegar argumentos query
     image = request.args.get("image_base64")
     image_name = request.args.get("image_name") # nome para salvar
-    # bucket.download_file(image, image) # nome no bucket, path/nome que sera salvo
     with open(image_name, "wb") as fh:
-        fh.write(base64.decodebytes(image.encode()))
+        fh.write(base64.decodebytes(image))
     analytics = VisionManager(image_name)
     analytics.execute_detect()
     image_name = f"output_image{time.time()}.jpg"
     analytics.save_image(f'data/{image_name}')
-    # bucket.upload_file(image_name, image_name)
-    return image_name
+    with open(f'data/{image_name}', "rb") as image_file:
+        response = base64.b64encode(image_file.read())
+    return response
 
 
 if __name__ == '__main__':
